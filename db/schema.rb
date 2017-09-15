7e5be682e895dca31e4864897bff529f25545971
Mike Hoitomt <mike.hoitomt@gmail.com>
2017-09-14 23:18:56 -0500

    Rebuild the schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,29 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140805014512) do
+ActiveRecord::Schema.define(version: 20161231172836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "pg_trgm"
 
-  create_table "api_keys", force: true do |t|
+  create_table "api_keys", force: :cascade do |t|
     t.string   "access_token"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "cash", force: true do |t|
+  create_table "cash", force: :cascade do |t|
     t.integer "tag_id"
     t.float   "amount"
   end
 
-  create_table "tags", force: true do |t|
+  create_table "tags", force: :cascade do |t|
     t.string "name",   limit: 50
     t.float  "amount"
   end
 
-  create_table "ticket_line_items", force: true do |t|
+  create_table "ticket_line_items", force: :cascade do |t|
     t.integer  "ticket_id"
     t.string   "away_team",        limit: 50
     t.integer  "away_score"
@@ -41,21 +41,23 @@ ActiveRecord::Schema.define(version: 20140805014512) do
     t.integer  "home_score"
     t.datetime "line_item_date"
     t.string   "line_item_spread", limit: 50
+    t.string   "description"
   end
 
-  create_table "ticket_tags", force: true do |t|
+  create_table "ticket_tags", force: :cascade do |t|
     t.integer "ticket_id"
     t.integer "tag_id"
     t.float   "amount"
   end
 
-  create_table "tickets", force: true do |t|
-    t.integer  "sb_bet_id"
+  create_table "tickets", force: :cascade do |t|
+    t.string   "sb_bet_id"
     t.datetime "wager_date"
     t.string   "wager_type",     limit: 50
     t.float    "amount_wagered"
     t.float    "amount_to_win"
     t.string   "outcome",        limit: 50
+    t.float    "amount_paid"
   end
 
 end
