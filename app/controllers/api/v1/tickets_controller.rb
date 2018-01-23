9a25025ed4183c6a5ba76cdfae12da1b1c3d3f24
Mike Hoitomt <mike.hoitomt@gmail.com>
2018-01-22 20:50:53 -0600

    Add scope where the results can be filtered by outcome

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -6,6 +6,7 @@ module Api
         scope = Ticket.includes(:ticket_line_items, ticket_tags: [:tag]).order('wager_date desc')
         scope = scope.where("wager_date >= ?", params[:start_date]) if params[:start_date]
         scope = scope.where("wager_date <= ?", params[:stop_date]) if params[:stop_date]
+        scope = scope.where("lower(outcome) in (?)", params[:outcome].split(',')) if params[:outcome]
         scope
         render json: scope
       end
